90b3c8fc9f6e7679ff5bf24b17e406d59a5876e1
Jen W <jsc525@gmail.com>
2019-06-27 16:31:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -129,9 +129,17 @@ function endGame() {
 
 function moveDodger(e) {
   // implement me!
+  /**	
+    * This function should call `moveDodgerLeft()`	  
+   * if the left arrow is pressed and `moveDodgerRight()`	    e.preventDefault()
+   * if the right arrow is pressed. (Check the constants	    e.stopPropagation()
+   * we've declared for you above.)	  }
+   * And be sure to use the functions declared below!	
+    */	  
     const code = e.which;
     if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
       e.preventDefault();
+      e.stopPropagation();
   /**	
     * This function should call `moveDodgerLeft()`	  
     * if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
