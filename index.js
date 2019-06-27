6b89ceda5517f19bf148664d66060ec3501b3c80
Jen W <jsc525@gmail.com>
2019-06-27 16:08:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -106,13 +106,11 @@ function createRock(x) {
      */
   }
 
-  window.requestAnimationFrame(step)
+  window.requestAnimationFrame(moveRock);
   // We should kick of the animation of the rock around here
-
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock);
-
   // Finally, return the rock element you've created
   return rock;
 }
