3af5d0747f3eca4b1912559f2b7c5df51e292a01
Jen W <jsc525@gmail.com>
2019-06-27 16:33:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ function moveDodgerRight() {
    */
    window.requestAnimationFrame(function() {
      const left = positionToInteger(DODGER.style.left);
-     if (left > 360) {
+     if (left < 360) {
        
      }
    });
