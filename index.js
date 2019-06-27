1be96cfd86aebc097527855f748f3be9fe399598
Jen W <jsc525@gmail.com>
2019-06-27 15:41:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,21 +57,22 @@ function checkCollision(rock) {
 }
 
 function createRock(x) {
-  const rock = document.createElement('div')
+  const rock = document.createElement('div');
 
-  rock.className = 'rock'
-  rock.style.left = `${x}px`
+  rock.className = 'rock';
+  rock.style.left = `${x}px`;
 
   // Hmmm, why would we have used `var` here?
-  var top = 0
+  var top = 0;
 
-  rock.style.top = top
+  rock.style.top = top;
 
   /**
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-
+   
+  GAME.appendChild(rock); 
 
   /**
    * This function moves the rock. (2 pixels at a time
