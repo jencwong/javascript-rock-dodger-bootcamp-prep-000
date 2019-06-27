9a490162e545cbf13506c7d74576c904b3eabd78
Jen W <jsc525@gmail.com>
2019-06-27 15:52:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -97,11 +97,7 @@ function createRock(x) {
      */
      if (top < GAME_HEIGHT) {
        window.requestAnimationFrame(moveRock);
-    }
-  }
- 
-  window.requestAnimationFrame(step)
-}
+   
        
      }
        
