0ce6bfab7b3355b70094f1daf97a346e70d0026b
Jen W <jsc525@gmail.com>
2019-06-27 15:52:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function createRock(x) {
      * the GAME, we want to move it again.
      */
      if (top < GAME_HEIGHT) {
-       window.requestAnimationFrame(step)
+       window.requestAnimationFrame(moveRock);
     }
   }
  
